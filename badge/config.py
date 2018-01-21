da5ae6add8e07c4564ed671e86a0f55f9ea9c3e0
Matt Gibbs <matt@invalidate.net>
2018-01-21 10:17:52 -0500

    Initial commit - 2018 ShmooCon badge deployed version

---
+++ badge/config.py
@@ -0,0 +1,38 @@
+# config.py
+# WarBadge Common Configuration
+
+# The Wi-Fi SSID to connect to
+CONNECT_SSID = 'shmoocon-romp'
+
+# The base URL to post scan results to
+UPLOAD_URL = 'https://warbadge.ninja/checkin/'
+
+# Don't upload more often than this in ms
+UPLOAD_INTERVAL = 20000
+
+# Status code for good upload
+UPLOAD_GOOD_STATUS = 201
+
+# Don't scan more often than this in ms
+SCAN_INTERVAL = 15000
+
+# NeoPixel LED configuration
+PIN_NEOPIXEL = 14
+
+# Number of LEDs in the chain
+LED_COUNT = 2
+
+# LED indices
+#   0 is top of rocket
+#   1 is bottom of rocket
+LED_SCAN = 0
+LED_WIFI = 1
+
+# LED color constants
+LED_COLOR_WIFI_DOWN = (0, 0, 255)
+LED_COLOR_WIFI_UP = (0, 255, 0)
+LED_COLOR_SCAN = (0, 0, 255)
+LED_COLOR_UPLOAD = (255, 100, 0)
+LED_COLOR_SUCCESS = (0, 255, 0)
+LED_COLOR_ERROR = (255, 0, 0)
+LED_COLOR_OFF = (0, 0, 0)
---
+++ badge/main.py
@@ -0,0 +1,128 @@
+# main.py
+# WarBadge main program
+
+import time
+import machine
+import neopixel
+import network
+import ubinascii
+import urequests
+import config
+
+# Scan for SSIDs
+def scan(nic, ap_neighbors):
+  # Scan for local APs
+  scan_results = nic.scan()
+
+  # Compare scan results against what we have already seen
+  for result in scan_results:
+    ssid = result[0]
+    bssid = ubinascii.hexlify(result[1])
+    rssi = result[3]
+    # Check whether this is a new SSID or a new BSSID/RSSI
+    if ssid not in ap_neighbors:
+      # A new SSID
+      ap_neighbors[ssid] = {
+        bssid: rssi
+      }
+    else:
+      # A new BSSID and/or RSSI
+      ap_neighbors[ssid][bssid] = rssi
+
+
+def main():
+  # Our main program setup
+
+  # Set up the two RGB LEDs
+  np = neopixel.NeoPixel(machine.Pin(config.PIN_NEOPIXEL), config.LED_COUNT)
+
+  # LEDs off
+  np.fill(config.LED_COLOR_OFF)
+  np.write()
+
+  # Set the Wi-Fi radio to be a client only
+  nic = network.WLAN(network.STA_IF)
+  nic.active(True)
+  unused_nic = network.WLAN(network.AP_IF)
+  unused_nic.active(False)
+
+  # Capture the MAC for the ID
+  mac_id = str(ubinascii.hexlify(nic.config('mac')), 'utf-8')
+
+  # Print the MAC ID on the console
+  print('You are %s' % mac_id)
+
+  # Keep track of all the APs we see
+  ap_neighbors = {}
+
+  # Keep track of how often we scan for SSIDs
+  last_scan = time.ticks_ms()
+
+  # Keep track of how often we upload scan results
+  last_upload = time.ticks_ms()
+
+  # Our main program loop
+  while True:
+    # Check Wi-Fi connectivity
+    if nic.status() == network.STAT_GOT_IP:
+      # Connected
+      np[config.LED_WIFI] = config.LED_COLOR_WIFI_UP
+      np.write()
+
+      # Check whether we should upload scan results yet
+      if time.ticks_diff(time.ticks_ms(), last_upload) >= config.UPLOAD_INTERVAL:
+        # It's time to upload
+        np[config.LED_SCAN] = config.LED_COLOR_UPLOAD
+        np.write()
+
+        # Print the MAC ID on the console
+        print('You are %s' % mac_id)
+
+        try:
+          response = urequests.post(config.UPLOAD_URL + mac_id, json=ap_neighbors,  headers={'Content-Type': 'application/json', 'User-Agent': 'WarBadge Experimental ShmooCon 2018'})
+        except:
+          # The upload didn't work
+          np[config.LED_SCAN] = config.LED_COLOR_ERROR
+          np.write()
+        else:
+          # Check response status
+          if response.status_code == config.UPLOAD_GOOD_STATUS:
+            # It worked
+            np[config.LED_SCAN] = config.LED_COLOR_SUCCESS
+            np.write()
+          else:
+            # Bad response
+            np[config.LED_SCAN] = config.LED_COLOR_ERROR
+            np.write()
+
+          response.close()
+
+        last_upload = time.ticks_ms()
+
+    elif nic.status() == network.STAT_CONNECTING:
+      # Still connecting
+      np[config.LED_WIFI] = config.LED_COLOR_WIFI_DOWN
+      np.write()
+    else:
+      # Need to connect
+      np[config.LED_WIFI] = config.LED_COLOR_WIFI_DOWN
+      np.write()
+      nic.connect(config.CONNECT_SSID)
+
+    # Check whether we should scan for SSIDs yet
+    if time.ticks_diff(time.ticks_ms(), last_scan) >= config.SCAN_INTERVAL:
+      # It's time to scan
+      np[config.LED_SCAN] = config.LED_COLOR_SCAN
+      np.write()
+      scan(nic, ap_neighbors)
+      last_scan = time.ticks_ms()
+      np[config.LED_SCAN] = config.LED_COLOR_OFF
+      np.write()
+
+    # Add a little delay to save battery
+    time.sleep_ms(50)
+
+
+# Run our program
+main()
+
