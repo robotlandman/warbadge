68c2148b64bafaf353c095f15b693a434847955d
ipl31 <ken@ipl31.net>
2018-01-22 08:24:40 -0500

    Initial commit of warbadge leaderboard

---
+++ leaderboard/warbadge_app/sql/entries.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `warbadge`.`entries` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `badge_mac` VARCHAR(45) NOT NULL,
+  `ssid` VARCHAR(64) NOT NULL,
+  `bssid_mac` VARCHAR(45) NOT NULL,
+  `rssi` BIGINT NOT NULL,
+  PRIMARY KEY (`id`));
